4b959f95659e8a5036c76143e104663ea38cd92d
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-12-08 19:34:59 +0900

    GitHub Pagesへデプロイする設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "REPOSITORY_NAME" : "./",
   plugins: [react()],
   resolve: {
     alias: {
